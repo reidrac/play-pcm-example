2125fb1e9fb21fb796eb2fc2a2534481c81a931b
reidrac <jjm@usebox.net>
2012-08-22 20:56:23 +0100

    first commit!

---
+++ play.ino
@@ -0,0 +1,77 @@
+/*
+ * Play example using Time3 library.
+ *
+ * The sample must be raw 8bits 8KHz, please read README.MD.
+ *
+ * References:
+ *
+ *  - http://arduino.cc/en/Tutorial/SecretsOfArduinoPWM
+ *  - http://www.arduino.cc/playground/Main/TimerPWMCheatsheet
+ *  - http://arduino.cc/playground/Code/Timer1
+ *  - http://www.pjrc.com/teensy/td_libs_TimerOne.html
+ *
+ *  Juan J. Martinez <jjm@usebox.net>
+ *  https://github.com/reidrac/play-pcm-example
+ */
+
+#include <avr/pgmspace.h>
+#include <TimerThree.h>
+
+/* sample to play */
+#include "coin8.h"
+
+volatile unsigned char *play_sample;
+volatile unsigned short play_sample_length;
+volatile unsigned short play_sample_ptr;
+volatile int play_pin;
+
+/* for Teensy 2.0: pin 9 */
+void init_play(unsigned char pin)
+{
+  play_sample_length = 0;
+  play_pin = pin;
+
+  pinMode(play_pin, OUTPUT);
+
+  // 8KHz are 125 microsencodns
+  Timer3.initialize(125);
+  Timer3.attachInterrupt(play_cb);
+  Timer3.pwm(pin, HIGH);
+}
+
+void play_cb(void)
+{
+  if(play_sample_length)
+  {
+    Timer3.setPwmDuty(play_pin, pgm_read_byte(&play_sample[play_sample_ptr]));
+    play_sample_ptr++;
+
+    if(play_sample_ptr == play_sample_length)
+    {
+      play_sample_length = 0;
+      Timer3.setPwmDuty(play_pin, 0);
+    }
+  }
+}
+
+void play(const unsigned char *sample, const unsigned short length)
+{
+  noInterrupts();
+  play_sample = (unsigned char *)sample;
+  play_sample_ptr = 0;
+  play_sample_length = length;
+  interrupts();
+}
+
+void setup()
+{
+  init_play(9);
+  play(coin8, coin8_len);
+}
+
+void loop()
+{
+  /* nothing */
+}
+
+/* EOF */
---
+++ tools/raw2c.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+    FILE *fd_in, *fd_out;
+    unsigned char byt;
+    char name[16], *p;
+    unsigned short length = 0;
+
+    if(argc!=3)
+    {
+        fprintf(stderr, "usage: %s infile.raw outfile.h\nThe raw file is expected to contain raw audio data (8bits 8KHz).\n", argv[0]);
+        return 1;
+    }
+
+    fd_in = fopen(argv[1], "r");
+    if(!fd_in)
+    {
+        fprintf(stderr, "failed to open %s\n", argv[1]);
+        return 1;
+    }
+
+    fd_out = fopen(argv[2], "w");
+    if(!fd_out)
+    {
+        fclose(fd_in);
+        fprintf(stderr, "failed to open %s\n", argv[2]);
+        return 1;
+    }
+
+    printf("Converting %s into %s...\n", argv[1], argv[2]);
+
+    for(byt=0, p=argv[2]; *p && *p!='.' && byt<16; byt++, p++)
+    {
+        name[byt] = *p;
+    }
+
+    name[byt] = 0;
+
+    fprintf(fd_out, "/* converted from %s */\n", argv[1]);
+    fprintf(fd_out, "const unsigned char %s[] PROGMEM = { ", name);
+
+    while(!feof(fd_in))
+    {
+       byt = fgetc(fd_in);
+       fprintf(fd_out, "0x%x", byt);
+       length++;
+       if(!feof(fd_in))
+        fprintf(fd_out, ", ");
+    }
+
+    fprintf(fd_out, " };\n");
+    fprintf(fd_out, "const unsigned short %s_len = %d;\n", name, length);
+
+    fclose(fd_in);
+    fclose(fd_out);
+
+    printf("done!\n");
+
+    return 0;
+}
